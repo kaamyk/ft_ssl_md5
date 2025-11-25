d828d2759b862fc740d342b00d2a64cb50ae19dc
Antoine VINCENT <anvincen@f4r11s5.clusters.42paris.fr>
2025-11-25 11:10:27 +0100

    Added -h

--- src/main.c
+++ src/main.c
@@ -230,7 +230,11 @@ void	print_args(char **argv)
 
 void	launch_algo(t_data data)
 {
-	if ((data.options >> 5) & 1)
+	if (data.options & USAGE)
+	{
+		write(STDOUT_FILENO, "ft_ssl usage:\n\techo string | ./ft_ssl algorithm [options] [-s \"string\"] arguments\nalgorithm values : selects the hash algorithm to run\n\tsha256\n\tmd5\noptions: \n\tsets the display format :\n\t\t-p : print stdin to stdout and append the checksum to stdout.\n\t\t-q : quiet mode\n\t\t-r : reverse the format of the ouput\n\t-s : print the sum of the given string. Any argument after this option is consider as a string. Each argument after the string is consider as a filename (see arguments section).\n\t-h : print usage\narguments :\n\t Each is considered has a file name. The command tries to open each file, is it fails it goes on.\n", 615);
+	}
+	else if (data.options & MD5)
 	{
 		MDString(data.options, data.pipe);
 		data.options &= ~(IS_PIPE);
@@ -240,7 +244,7 @@ void	launch_algo(t_data data)
 			data.inputs++;
 		}
 	}
-	// else
+	// else if (data.options & SHA256)
 	// 	SHAString(input);
 }
 
